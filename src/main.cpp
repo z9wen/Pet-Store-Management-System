245b238cd3d8f5a5762b8d0875d68c75e26e4278
Will <35718723+z9wen@users.noreply.github.com>
2024-10-03 16:54:26 +1000

    update CMakelists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ auto plus(int a, int b) -> int {
     return result;
 }
 
-#if 0 //for test 1 on & 0 off
+#if 1 //for test 1 on & 0 off
 auto main() -> int {
     std::cout << "Welcome to Pet Store Management System" << std::endl;
 

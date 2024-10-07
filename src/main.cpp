d02a9f9e6ea1745fb3d47c3c90c8db6531d3273c
Will <35718723+z9wen@users.noreply.github.com>
2024-10-07 18:46:31 +1100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -34,10 +34,10 @@ auto main() -> int {
 	printAscii();
 
 	std::string dbName = "pet_store_db";
-	// 检查数据库是否存在
+
 	if (!checkDatabaseExists(dbName)) {
 		std::cout << "Database does not exist. Initializing..." << std::endl;
-		getUserInputAndCreateDatabase(); // 数据库不存在时创建
+		getUserInputAndCreateDatabase();
 	}
 	else {
 		std::cout << "Database already exists." << std::endl;
